fafe33a19e4b25c29feb755d73d57d3fd7452d4a
Inndy <Inndy.mail@gmail.com>
2014-11-19 12:18:39 +0800

    Add AobScanner

--- Functions.cpp
+++ Functions.cpp
@@ -1,10 +1,34 @@
 //---------------------------------------------------------------------------
-
+#include <cstring>
 #pragma hdrstop
 
 #include "Functions.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
+
+using namespace std;
+
+char inline is_hex(char ch) {
+    return ('0' <= ch && ch <= '9') ||
+           ('A' <= ch && ch <= 'F') ||
+           ('a' <= ch && ch <= 'f');
+}
+
+char inline hex2int(char ch) {
+    if ('0' <= ch && ch <= '9') {
+        return ch - '0';
+    } else if ('A' <= ch && ch <= 'F') {
+        return ch - 'A' + 0xA;
+    } else if ('a' <= ch && ch <= 'f') {
+        return ch - 'a' + 0xA;
+    } else {
+        return '\0';
+    }
+}
+
+char inline is_fuzzy(char ch) {
+    return (ch == '?' || ch == '.' || ch == '*') ? '?' : '\0';
+}
 
 void Press(int k)
 {
@@ -40,5 +64,80 @@ namespace Hack
     DWORD MemoryRegion::QuerySize(char *adr)
     {
         return this->QuerySize((void*)adr);
+    }
+
+    // AobScanner
+
+    AobScanner::AobScanner(void *buffer, unsigned long size)
+    {
+        this->buffer = (char *)buffer;
+        this->size = size;
+    }
+
+    AobScanner::AobScanner(char *buffer, unsigned long size)
+    {
+        this->buffer = buffer;
+        this->size = size;
+    }
+
+    char *AobScanner::Scan(const char *feature, bool alignment, long offset)
+    {
+        char *buffer = this->buffer, *end = this->buffer + this->size;
+        long feature_length, t = strlen(feature);
+        char *feature_bytes = new char[t],
+             *feature_mask = new char[t];
+        bool found = false;
+        int i;
+
+        this->ParseFeature(feature, feature_bytes, feature_mask, &feature_length);
+
+        end -= feature_length;
+
+        while (buffer < end && !found) {
+            found = true;
+            for (i = 0; i < feature_length; i++) {
+                if ((buffer[i] & feature_mask[i]) != feature_bytes[i]) {
+                    found = false;
+                    break;
+                }
+            }
+            if (!found) {
+                buffer++;
+            }
+        }
+
+        delete [] feature_bytes;
+        delete [] feature_mask;
+
+        return found ? buffer + offset : NULL;
+    }
+
+    void AobScanner::ParseFeature(const char *feature, char *out, char *mask, long *len)
+    {
+        enum ParseState { LO, HI };
+        ParseState state = HI;
+        *len = 0;
+        while (*feature) {
+            bool is_digit = is_hex(*feature);
+            if (state == HI) {
+                *out = 0;
+                *mask = 0;
+            }
+            if (is_digit) {
+                *out |= hex2int(*feature) << (state == HI ? 4 : 0);
+                *mask |= state == HI ? 0xF0 : 0x0F;
+            }
+            if (is_digit || is_fuzzy(*feature)) {
+                if (state == LO) {
+                    state = HI;
+                    out++;
+                    mask++;
+                    (*len)++;
+                } else {
+                    state = LO;
+                }
+            }
+            feature++;
+        }
     }
 }
--- Functions.h
+++ Functions.h
@@ -16,5 +16,16 @@ namespace Hack {
         private:
             void *next_region;
     };
+
+    class AobScanner {
+        public:
+            AobScanner(void *buffer, unsigned long size);
+            AobScanner(char *buffer, unsigned long size);
+            char *Scan(const char *feature, bool alignment = false, long offset = 0);
+            void ParseFeature(const char *feature, char *out, char *mask, long *len);
+        private:
+            char *buffer;
+            unsigned long size;
+    };
 }
 #endif
